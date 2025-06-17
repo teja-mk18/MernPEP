60c5f02e8bce8c7909b724a1d5658962db5cda3a
teja <saisanthoshkteja@gmail.com>
2025-06-17 10:00:52 +0530

    initial commit

---
+++ day1/app.js
@@ -0,0 +1,11 @@
+// window.console.log("Hello from JS!");
+// //let, consr OR var;
+// console.log(age);
+// var age = 21; //declaration and initialization
+// console.log(age);
+// var age = 24; //re-declaration
+// console.log(age);
+let age = 22;
+console.log(age);
+age = 24;
+console.log(age);
---
+++ day2/part1/app.js
@@ -0,0 +1,34 @@
+// console.log("Hello from JS!");
+// const printBill = function(txt) {
+//     console.log("--------------------------");
+//     console.log("Rs.", txt);
+//     console.log("--------------------------");
+// };
+
+const printBill = (txt) => {
+    console.log("--------------------------");
+    console.log("Rs.",txt);
+    console.log("--------------------------");
+};
+
+const calculateBillAmountForFood = (price) => {
+    return price * 1.05;
+};
+const calculateBillAmountForClothes = (price) => {
+    return price * 1.12;
+};
+const calculateBillAmountForDrinks = (price) => {
+    return price * 1.2;
+};
+// Higher order function -> a function that takes other functions as arguments or returns a function
+// Callback function -> a function that is passed as an argument to another function and is executed later
+
+const genarateBill = (food, clothes,drinks) => {
+    const foodAmount = calculateBillAmountForFood(food);
+    const clothesAmount = calculateBillAmountForClothes(clothes);
+    const drinksAmount = calculateBillAmountForDrinks(drinks);
+    const totalAmount = foodAmount + clothesAmount + drinksAmount;
+    printBill(totalAmount);
+};
+
+genarateBill(100, 400, 100);
